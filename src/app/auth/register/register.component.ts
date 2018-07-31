a28fe4459d94778c893cdcdf5ed48bde212bd6e2
Rihards Veters <rihards@Rihardss-MacBook-Pro.local>
2018-07-31 22:51:41 +0300

    Adds update feature. Adds update page. Adds rental guard. Adds endpoint to patch rental. Adds owner adds panel

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -24,7 +24,6 @@ export class RegisterComponent implements OnInit {
       },
       (errorResponse) => {
         this.errors = errorResponse.error.errors;
-        
       }
     )
   }
---
+++ src/app/rental/rental-update/owner-adds/owner-adds.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bwm-owner-adds',
+  templateUrl: './owner-adds.component.html',
+  styleUrls: ['./owner-adds.component.scss']
+})
+export class OwnerAddsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
