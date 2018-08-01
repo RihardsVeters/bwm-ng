c15514b898d83d852c64b8ee095e48be77be16f1
Rihards Veters <rihards@Rihardss-MacBook-Pro.local>
2018-08-01 23:12:30 +0300

    Updates angluar5 to angular6

--- src/app/manage/manage-rental/manage-rental.component.ts
+++ src/app/manage/manage-rental/manage-rental.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { RentalService } from '../../rental/shared/rental.service';
 import { Rental } from '../../rental/shared/rental.model';
-import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { ToastrService } from 'ngx-toastr';
 import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
@@ -13,9 +13,7 @@ export class ManageRentalComponent implements OnInit {
   rentals: Rental[];
   rentalDeleteIndex: number;
   constructor(private rentalService: RentalService,
-              private toastr: ToastsManager,
-              private vcr: ViewContainerRef) { 
-      this.toastr.setRootViewContainerRef(vcr);
+              private toastr: ToastrService) { 
   }
 
   ngOnInit() {
