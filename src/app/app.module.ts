49b585f800b8a015553bd30e171cda3945e40ced
Rihards Veters <rihardsveters@Rihardss-MacBook-Pro.local>
2018-11-21 14:24:22 +0200

    Adds stripe payments and simple user profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrModule } from 'ngx-toastr';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
@@ -14,8 +12,7 @@ import { RentalComponent } from './rental/rental.component';
 import { RentalModule } from './rental/rental.module';
 import { AuthModule } from './auth/auth.module';
 import { ManageModule } from './manage/manage.module';
-
-
+import { UserModule } from './user/user.module';
 
 const routes: Routes = [
   {path: '', redirectTo: '/rentals', pathMatch :'full'},
@@ -34,7 +31,8 @@ const routes: Routes = [
     NgbModule.forRoot(),
     ToastrModule.forRoot(),
     BrowserAnimationsModule,
-    ManageModule
+    ManageModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
